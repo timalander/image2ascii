68d05ea42ee29c9e1ecd8ba913225ca57a91ddb1
Tim Alander <tim.alander@gmail.com>
2014-04-03 19:01:20 -0400

    Conversion to grayscale fixed

--- pta.py
+++ pta.py
@@ -29,11 +29,11 @@ def open_image():
 def convert_to_grayscale(img):
     pixel_data = img.load()
     if len(img.split()) == 4:
-        for y in xrange(img.size[1]): # For each row ...
-            for x in xrange(img.size[0]): # Iterate through each column ...
+        for y in xrange(img.size[1]):
+            for x in xrange(img.size[0]):
                 # Check if it's opaque
                 if pixel_data[x, y][3] < 255:
-                    # Replace the pixel data with the colour white
+                    # Replace the pixel data with the color white
                     pixel_data[x, y] = (255, 255, 255, 255)
     img = img.convert("L")
     img.save("out.bmp")
