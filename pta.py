6a4f5991f4b842cb91d58b943e1dba57491a9423
Tommy Folliard <tfolliard@yahoo.com>
2014-04-05 00:32:37 -0400

    Added invert option

--- pta.py
+++ pta.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 #coding: utf-8
 
 import sys
 import os
 from PIL import Image
-
+import PIL.ImageOps
 
 def main():
    img = open_image()
@@ -12,23 +12,27 @@ def main():
    ascii_img_array = convert_to_ascii(img)
    display(ascii_img_array)
 
-
 def open_image():
+    isInvert = 0
+    fileArg = 1
     if len(sys.argv) == 1:
-        print "No file specified"
-        sys.exit(1)
-    
+        sys.exit("No file specified")
     elif len(sys.argv) > 2:
-        print "Please only specify one file to convert"
-        sys.exit(1)
-    
-    root,ext = os.path.splitext(sys.argv[1])
-    if ext.lower() not in ['.jpg', '.jpeg', '.png', '.bmp']:
-        print "Not a valid image file"
-        sys.exit(1)
-    else:
-        return Image.open(sys.argv[1])
+        if sys.argv[1] == "-i":
+            isInvert = 1
+            fileArg = 2
+        else:
+            sys.exit("Please only specify one file to convert")
 
+    root,ext = os.path.splitext(sys.argv[fileArg])
+    if ext.lower() not in ['.jpg', '.jpeg', '.png', '.bmp']:
+        sys.exit("Not a valid image file")
+    else:
+        image = Image.open(sys.argv[fileArg])
+        if isInvert:
+            return PIL.ImageOps.invert(image)
+        else:
+            return image
 
 def convert_to_grayscale(img):
     pixel_data = img.load()
@@ -44,8 +48,8 @@ def convert_to_grayscale(img):
 def convert_to_ascii(img):
     # symbol_array = ['@', '$', 'B', '%', '8', '&', 'W', 'M', '#', '*', 'o', 'a',
     #      'h', 'k', 'b', 'd', 'p', 'q', 'w', 'm', 'Z', 'O', '0', 'Q', 'L', 'C',
-    #      'J', 'U', 'Y', 'X', 'z', 'c', 'v', 'u', 'n', 'x', 'r', 'j', 'f', 't', 
-    #      '/', '|', '(', ')', '1', '{', ']', '?', '-', '+', '~', '>', 'i', '!', 
+    #      'J', 'U', 'Y', 'X', 'z', 'c', 'v', 'u', 'n', 'x', 'r', 'j', 'f', 't',
+    #      '/', '|', '(', ')', '1', '{', ']', '?', '-', '+', '~', '>', 'i', '!',
     #      'l', 'I', ';', ':', ',', '"', '^', '`', '.', ' ']
     symbol_array = ['@', '%', '#', '*', '+', '=', '-', ':', '.']
     pixel_data = img.load()
