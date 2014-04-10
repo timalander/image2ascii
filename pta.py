aa5750d9a5405d00e32288ce2562e51c3486f08a
Tim Alander <tim.alander@gmail.com>
2014-04-09 21:56:40 -0400

    Added argparse and removed redundant convert_to_ascii_reddit function

--- pta.py
+++ pta.py
@@ -3,33 +3,30 @@
 
 import sys
 import os
+import argparse
 from PIL import Image
 import PIL.ImageOps
 
 def main():
-   img = open_image()
-   img = convert_to_grayscale(img)
-   ascii_img_array = convert_to_ascii(img)
-   display(ascii_img_array)
+    img = open_image()
+    img = convert_to_grayscale(img)
+    ascii_img_array = convert_to_ascii(img)
+    display(ascii_img_array)
 
 def open_image():
-    isInvert = 0
-    fileArg = 1
-    if len(sys.argv) == 1:
-        sys.exit("No file specified")
-    elif len(sys.argv) > 2:
-        if sys.argv[1] == "-i":
-            isInvert = 1
-            fileArg = 2
-        else:
-            sys.exit("Please only specify one file to convert")
-
-    root,ext = os.path.splitext(sys.argv[fileArg])
+    parser = argparse.ArgumentParser(description='Convert images to ASCII')
+    parser.add_argument('-i', action="store_true", help="Invert image colors" , dest="is_invert")
+    parser.add_argument('-r', action="store_true", default=False, help="Output in reddit comment formatting" , dest="is_reddit")
+    parser.add_argument('filename', action="store", type=str)
+    args = parser.parse_args()
+    global is_reddit 
+    is_reddit = args.is_reddit
+    root,ext = os.path.splitext(args.filename)
     if ext.lower() not in ['.jpg', '.jpeg', '.png', '.bmp']:
         sys.exit("Not a valid image file")
     else:
-        image = Image.open(sys.argv[fileArg])
-        if isInvert:
+        image = Image.open(args.filename)
+        if args.is_invert:
             return PIL.ImageOps.invert(image)
         else:
             return image
@@ -44,6 +41,7 @@ def convert_to_grayscale(img):
                     # Replace the pixel data with the color white
                     pixel_data[x, y] = (255, 255, 255, 255)
     return img.convert("L")
+
 
 def convert_to_ascii(img):
     # symbol_array = ['@', '$', 'B', '%', '8', '&', 'W', 'M', '#', '*', 'o', 'a',
@@ -60,6 +58,8 @@ def convert_to_ascii(img):
     ascii_img_array = []
     for i in xrange(0, height, block_height):
         row = []
+        if is_reddit:
+            row.append('`')
         if i+block_height <= height:
             for j in xrange(0, width, block_width):
                 avg = 0;
@@ -69,37 +69,11 @@ def convert_to_ascii(img):
                             avg += pixel_data[x, y]
                     avg = avg/(block_height*block_width)
                     row.append(symbol_array[int(round((avg/255.0)*(len(symbol_array)-1)))])
+            if is_reddit:
+                row.append('`  ')
             ascii_img_array.append(row)
     return ascii_img_array
 
-def convert_to_reddit_ascii(img):
-    # symbol_array = ['@', '$', 'B', '%', '8', '&', 'W', 'M', '#', '*', 'o', 'a',
-    #      'h', 'k', 'b', 'd', 'p', 'q', 'w', 'm', 'Z', 'O', '0', 'Q', 'L', 'C',
-    #      'J', 'U', 'Y', 'X', 'z', 'c', 'v', 'u', 'n', 'x', 'r', 'j', 'f', 't',
-    #      '/', '|', '(', ')', '1', '{', ']', '?', '-', '+', '~', '>', 'i', '!',
-    #      'l', 'I', ';', ':', ',', '"', '^', '`', '.', ' ']
-    symbol_array = ['@', '%', '#', '*', '+', '=', '-', ':', '.']
-    pixel_data = img.load()
-    height = img.size[1]
-    width = img.size[0]
-    block_height = 2
-    block_width = 1
-    ascii_img_array = []
-    for i in xrange(0, height, block_height):
-        row = []
-        row.append('`')
-        if i+block_height <= height:
-            for j in xrange(0, width, block_width):
-                avg = 0;
-                if j+block_width <= width:
-                    for y in xrange(i, i+block_height):
-                        for x in xrange(j, j+block_width):
-                            avg += pixel_data[x, y]
-                    avg = avg/(block_height*block_width)
-                    row.append(symbol_array[int(round((avg/255.0)*(len(symbol_array)-1)))])
-            row.append('`  ')
-            ascii_img_array.append(row)
-    return ascii_img_array
 
 def display(ascii_img_array):
     for x in xrange(len(ascii_img_array)):
